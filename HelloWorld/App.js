53d42e22c980605169a5bbc8ff71b41c23880d1d
Dagmar87 <jdfssobrinho@hotmail.com>
2022-08-01 16:14:23 -0300

    primeira app finalizada

---
+++ HelloWorld/App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello World!!!</Text>
+      <Text style={styles.subtitle}>Bem-vindo ao Mundo do React Native!!!</Text>
+      <Text>Feito por José Dagmar</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title:{
+    fontSize:28
+  },
+  subtitle:{
+    fontSize:20
+  }
+});
